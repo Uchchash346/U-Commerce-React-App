b30492f3ea3b8813d00f28c2d5de85ca84c4195c
Uchchash346 <mru416@gmail.com>
2022-01-09 07:50:37 +0600

    Routing Setup

---
+++ src/pages/ProductInfo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function ProductInfo() {
+    return (
+        <Layout>
+            <h1>Product Info</h1>
+        </Layout>
+    )
+}
+
+export default ProductInfo
