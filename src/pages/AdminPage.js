29e2911f4cf6544f3456cfe042f2a3b698aaaa7f
Uchchash346 <mru416@gmail.com>
2022-01-14 12:18:03 +0600

    admin page completed

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,13 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import Layout from '../components/Layout'
-import { collection, addDoc, getDocs } from "firebase/firestore";
-import fireDB from '../fireConfig';
-import { FaEdit, FaTrash } from 'react-icons/fa';
-
+import React, { useEffect, useState } from "react";
+import Layout from "../components/Layout";
+import {
+    collection,
+    addDoc,
+    getDocs,
+    setDoc,
+    doc,
+    deleteDoc,
+} from "firebase/firestore";
+import fireDB from "../fireConfig";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import { Modal, Tabs, Tab } from "react-bootstrap";
+import { toast } from "react-toastify";
 
 function AdminPage() {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [product, setProduct] = useState({
+        title: "",
+        price: 0,
+        image: "",
+        category: "",
+    });
+    const [show, setShow] = useState(false);
+    const [add, setAdd] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [orders, setOrders] = useState([]);
+
 
     useEffect(() => {
         getData()
@@ -35,36 +55,222 @@ function AdminPage() {
             setLoading(false);
         }
     }
+
+    useEffect(() => {
+        getOrdersData();
+    }, []);
+
+    async function getOrdersData() {
+        try {
+            setLoading(true);
+            const result = await getDocs(collection(fireDB, "orders"));
+            const ordersArray = [];
+            result.forEach((doc) => {
+                ordersArray.push(doc.data());
+                setLoading(false);
+            });
+            console.log(ordersArray);
+            setOrders(ordersArray);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
+
+    const editHandler = (item) => {
+        setProduct(item)
+        setShow(true)
+    }
+
+    const updateProduct = async () => {
+        try {
+            setLoading(true);
+            await setDoc(doc(fireDB, "products", product.id), product);
+
+            handleClose();
+            toast.success("Product updated successfully");
+            window.location.reload();
+        } catch (error) {
+            toast.error("Product update failed");
+            setLoading(false);
+        }
+    };
+
+
+    const addProduct = async () => {
+        try {
+            setLoading(true);
+            await addDoc(collection(fireDB, "products"), product);
+            handleClose();
+            toast.success("Product added successfully");
+            window.location.reload();
+        } catch (error) {
+            toast.error("Product add failed");
+            setLoading(false);
+        }
+    };
+
+    const addHandler = () => {
+        setAdd(true);
+        handleShow();
+    };
+
+    const deleteProduct = async (item) => {
+        try {
+            setLoading(true);
+            await deleteDoc(doc(fireDB, "products", item.id));
+            toast.success("Product deleted successfully");
+            getData();
+        } catch (error) {
+            toast.failed("Product delete failed");
+            setLoading(false);
+        }
+    };
+
     return (
         <Layout loading={loading}>
-            <h3>Products List</h3>
-            <table className="table mt-3">
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Name</th>
-                        <th>Category</th>
-                        <th>Price</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map(item => {
-                        return (
+            <Tabs
+                defaultActiveKey="products"
+                id="uncontrolled-tab-example"
+                className="mb-3"
+            >
+                <Tab eventKey="products" title="Products">
+                    <div className="d-flex justify-content-between">
+                        <h3>Products List</h3>
+                        <button onClick={addHandler}>ADD PRODUCT</button>
+                    </div>
+                    <table className="table mt-3">
+                        <thead>
                             <tr>
-                                <td><img src={item.image} height="80" width="80" alt="" /></td>
-                                <td>{item.title}</td>
-                                <td>{item.category}</td>
-                                <td>{item.price}</td>
-                                <td>
-                                    <FaTrash />
-                                    <FaEdit />
-                                </td>
+                                <th>Image</th>
+                                <th>Name</th>
+                                <th>Category</th>
+                                <th>Price</th>
+                                <th>Action</th>
                             </tr>
-                        )
+                        </thead>
+                        <tbody>
+                            {products.map((item) => {
+                                return (
+                                    <tr>
+                                        <td>
+                                            <img src={item.image} height="80" width="80" />
+                                        </td>
+
+                                        <td>{item.title}</td>
+                                        <td>{item.category}</td>
+                                        <td>{item.price}</td>
+                                        <td>
+                                            <FaTrash
+                                                color="red"
+                                                size={20}
+                                                onClick={() => {
+                                                    deleteProduct(item);
+                                                }}
+                                            />
+
+                                            <FaEdit
+                                                onClick={() => editHandler(item)}
+                                                color="blue"
+                                                size={20}
+                                            />
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>
+                                {add === true ? "Add a product" : "Edit Product"}
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {" "}
+                            <div className="register-form">
+                                <input
+                                    type="text"
+                                    value={product.title}
+                                    className="form-control"
+                                    placeholder="name"
+                                    onChange={(e) =>
+                                        setProduct({ ...product, title: e.target.value })
+                                    }
+                                />
+                                <input
+                                    type="text"
+                                    value={product.image}
+                                    placeholder="image url"
+                                    className="form-control"
+                                    onChange={(e) =>
+                                        setProduct({ ...product, image: e.target.value })
+                                    }
+                                />
+                                <input
+                                    type="number"
+                                    value={product.price}
+                                    className="form-control"
+                                    placeholder="price"
+                                    onChange={(e) =>
+                                        setProduct({ ...product, price: e.target.value })
+                                    }
+                                />
+                                <input
+                                    type="text"
+                                    value={product.category}
+                                    className="form-control"
+                                    placeholder="category"
+                                    onChange={(e) =>
+                                        setProduct({ ...product, category: e.target.value })
+                                    }
+                                />
+
+                                <hr />
+                            </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <button onClick={handleClose}>Close</button>
+                            {add ? (
+                                <button onClick={addProduct}>SAVE</button>
+                            ) : (
+                                <button onClick={updateProduct}>SAVE</button>
+                            )}
+                        </Modal.Footer>
+                    </Modal>
+                </Tab>
+                <Tab eventKey="orders" title="Orders">
+                    {orders.map((order) => {
+                        return (
+                            <table className="table mt-3 order">
+                                <thead>
+                                    <tr>
+                                        <th>Image</th>
+                                        <th>Name</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {order.cartItems.map((item) => {
+                                        return (
+                                            <tr>
+                                                <td>
+                                                    <img src={item.image} height="80" width="80" />
+                                                </td>
+
+                                                <td>{item.title}</td>
+                                                <td>{item.price}</td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
+                        );
                     })}
-                </tbody>
-            </table>
+                </Tab>
+                <Tab eventKey="contact" title="Users" disabled></Tab>
+            </Tabs>
         </Layout>
     )
 }
